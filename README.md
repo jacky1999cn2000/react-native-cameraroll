66118882ba8b62e8f9cdde59521b7e5dee53cb11
Jacky Zhao <jacky.zhao@bunchball.com>
2016-06-13 23:23:50 +0800

    continuous load

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * [使用Dimensions来获取device的width and height](http://reactkungfu.com/2015/07/going-mobile-with-react-native/)
 * [如何listen手机的rotation](https://www.npmjs.com/package/react-native-orientation)
 * [如何disable手机的rotation](http://stackoverflow.com/questions/32176548/how-to-disable-rotation-in-react-native)
+* [如果disable warning](http://stackoverflow.com/questions/35309385/how-do-you-hide-the-warnings-in-react-native-ios-simulator)
--- index.ios.js
+++ index.ios.js
@@ -17,11 +17,16 @@ import {
   TouchableHighlight
 } from 'react-native';
 
+console.disableYellowBox = true;
+console.warn('YellowBox is disabled.');
+
 class cameraroll extends Component {
 
   constructor(props) {
      super(props);
      this.state = {
+       cursor: null,
+       hasMore: true,
        images: [],
        selected: []
      };
@@ -36,10 +41,16 @@ class cameraroll extends Component {
 
 
   storeImages = (data) => {
+    // console.log('data ',data);
     const assets = data.edges;
+    // assets.forEach((asset) => {
+    //   console.log('node ',asset.node);
+    // })
     const images = assets.map((asset) => asset.node.image);
     this.setState({
-      images: images
+      images: this.state.images.concat(images),
+      hasMore: data.page_info.has_next_page,
+      cursor: data.page_info.end_cursor
     });
   }
 
@@ -55,12 +66,26 @@ class cameraroll extends Component {
     console.log('selected',this.state.selected);
   }
 
-  render() {
+  loadMore = () => {
+    if(this.state.hasMore){
+      const fetchParams = {
+        first: 24,
+        after: this.state.cursor
+      };
+      CameraRoll.getPhotos(fetchParams).done(this.storeImages,this.logImageError);
+    }
+  }
 
+  render() {
+    console.log('this.state.images ',this.state.images);
     let imageSize = Dimensions.get('window').width/3;
+    console.log('imageSize ',imageSize);
 
     return (
-      <ScrollView style={styles.container}>
+      <ScrollView style={styles.container}
+        onScroll={this.loadMore}
+        scrollEventThrottle={0}
+      >
         <View style={styles.imageGrid}>
           {this.state.images.map((image) => {
               return (
